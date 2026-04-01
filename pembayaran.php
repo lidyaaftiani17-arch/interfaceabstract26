a610dac61c36370192b8468cf1db4c56bb156411
lidyaaftiani17-arch <lidyaaftiani17@gmail.com>
2026-04-01 15:59:27 +0700

    update pembayaran

--- pembayaran.php
+++ pembayaran.php
@@ -1,5 +1,5 @@
 <?php
-#Penggunaan Abstrak Class
+# Penggunaan Abstract Class
 abstract class Pembayaran {
     protected $jumlah;
 
@@ -13,6 +13,22 @@ abstract class Pembayaran {
     // method umum
     public function validasi() {
         return $this->jumlah > 0;
+    }
+
+    // diskon 10%
+    public function diskon() {
+        return $this->jumlah * 0.10;
+    }
+
+    // pajak 11%
+    public function pajak() {
+        return $this->jumlah * 0.11;
+    }
+
+    // total akhir
+    public function totalBayar() {
+        $total = $this->jumlah - $this->diskon();
+        return $total + $this->pajak();
     }
 }
 ?>
